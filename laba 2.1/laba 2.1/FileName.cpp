1748af8b3b8f372b4b841b70ca356a1caed0e132
vladyslav1223445 <vladkovalskij46@gmail.com>
2024-09-17 15:08:10 +0300

    12345

---
+++ laba 2.1/laba 2.1/FileName.cpp
@@ -0,0 +1,30 @@
+﻿// Lab_02.cpp
+// <Ковальський Владислав>
+// Лабораторна робота № 2
+// Лінійні програми
+// Варіант 7
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;    // вхідний параметр
+	double y;    // вхідний параметр
+	double z1;   // результат обчислення 1-го виразу
+	double z2;   // результат обчислення 2-го виразу
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	z1 = cos ^ 2(3 / 8pi - a / 4) - cos ^ 2(11 / 2pi + a / 4)
+		//z2 = √2 / 2 * sina / 2
+
+		cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
